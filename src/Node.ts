dbd45ad2af7ee25fa560aba2b2b51fc078ee7f19
Jan Steikowski <j.steikowski@aol.com>
2023-07-11 16:20:42 +0700

    Add Node class and Tree file

---
+++ src/Node.ts
@@ -0,0 +1,10 @@
+class NoDe {
+  data: any;
+  left: any;
+  right: any;
+  constructor(data: any, left: any, right: any) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
